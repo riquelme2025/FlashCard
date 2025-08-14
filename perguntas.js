4a634ec7c6d5dfa7cf8bb539070abc6bd3ace0bb
riquelme2025 <riquelme.matos@escola.pr.gov.br>
2025-08-14 14:45:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    '1889.'
+)
+
+criaCartao(
+    'História',
+    'Quem foi o líder da Inconfidência Mineira?',
+    'Tiradentes.'
+)
+
+criaCartao(
+    'História',
+    'Qual país foi responsável pelo início da Segunda Guerra Mundial?',
+    'Alemanha.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior bioma brasileiro em extensão?',
+    'Amazônia.'
+)
+
+criaCartao(
+    'Geografia',
+    'O que é uma fossa tectônica?',
+    'Uma depressão profunda formada entre placas tectônicas.'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são os pontos cardeais?',
+    'Norte, Sul, Leste e Oeste.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função dos pulmões no corpo humano?',
+    'Realizar as trocas gasosas entre oxigênio e gás carbônico.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Processo pelo qual as plantas produzem seu próprio alimento usando luz solar.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual estrutura celular é responsável pela produção de energia?',
+    'Mitocôndria.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a unidade básica da vida?',
+    'A célula.'
+)
